cbbaa083cbc2b4c1dd3a14e40817fc5d5bb13c6d
Pratyush Bansal <pratyushbansal05@gmail.com>
2026-03-19 08:36:44 +0530

    Implemented CLR1 parser

---
+++ src/CLR1.cpp
@@ -0,0 +1,32 @@
+#include "CLR1.hpp"
+#include "LRCommon.hpp"
+
+namespace SLR1 {
+
+ParsingTable generate(Grammar& grammar) {
+    LRCommon::CanonicalCollection cc = LRCommon::buildLR0Collection(grammar);
+    ParsingTable table;
+    LRCommon::populateShiftsAndGotos(table, cc, grammar);
+    
+    // Reductions: only on symbols in FOLLOW(A) for rule A -> alpha .
+    for (size_t i = 0; i < cc.states.size(); ++i) {
+        for (const auto& item : cc.states[i]) {
+            const Rule& rule = grammar.rules[item.ruleId];
+            if (item.dotPosition != (int)rule.rhs.size()) continue;
+            
+            if (rule.lhs == grammar.augmentedStart) {
+                table.addAction(i, "$", {ActionType::ACCEPT, 0});
+            } else {
+                const auto& follow = grammar.followSets[rule.lhs];
+                for (const auto& t : follow) {
+                    table.addAction(i, t, {ActionType::REDUCE, item.ruleId});
+                }
+            }
+        }
+    }
+    
+    table.detectConflicts();
+    return table;
+}
+
+}
