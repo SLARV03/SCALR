872213b7ed8936e5c9148b7bed5ec40d07066535
viivii <67176742+okayydiya@users.noreply.github.com>
2026-03-19 09:45:55 +0530

    LALR1 updated

---
+++ include/LALR1.hpp
@@ -0,0 +1,12 @@
+#ifndef LALR1_HPP
+#define LALR1_HPP
+
+#include "Shared_Structs.hpp"
+
+namespace LALR1 {
+
+ParsingTable generate(Grammar& grammar);
+
+} // namespace LALR1
+
+#endif // LALR1_HPP
